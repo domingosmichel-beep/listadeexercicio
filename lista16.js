1cc3f570f60a97b68214da629229c95834e07c67
Shadow Soft Maid <domingos.michel@escola.pr.gov.br>
2026-03-03 13:27:21 +0000

    listadeexercicios

---
+++ lista16.js
@@ -0,0 +1,11 @@
+let a = 3
+let b = 4
+let c = 5
+if(a===b && b===c){
+    console.log('equilátero')
+}else if(a===b || a===c || b===c){
+    console.log('isóceles')
+}else{
+    console.log('escaleno')
+}
+
