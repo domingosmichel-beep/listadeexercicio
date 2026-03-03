2199d5d3904cdaa9bf68c7dd7d1d7ef6fdde4fb0
Shadow Soft Maid <domingos.michel@escola.pr.gov.br>
2026-03-03 12:20:23 +0000

    listadeexercicios

---
+++ lista14.js
@@ -0,0 +1,7 @@
+let compra = 155;
+let desconto = compra * 0.95;
+if(compra > 100){
+    console.log(`o valor total de sua compra é de ${desconto} Reais`)
+}else{
+    console.log(`o valor da sua compra é de ${compra} Reais`)
+}
---
+++ lista15.js
@@ -0,0 +1,9 @@
+let pessoa = 19
+if(pessoa <= 11){
+    console.log('essa pessoa é de menor')
+}else if(pessoa > 11 & pessoa < 18) {
+    console.log('essa pessoa é um adolescente')
+}else{
+    console.log('essa pessoa é de maior')
+}
+
